fec376e0c7caaad23f6d0fbdbd295035e9a09895
stacy <stacydn23@gmail.com>
2021-10-14 01:05:51 -0400

    Done.

---
+++ build/precache-manifest.ac28ed8775e1eac6dc1fe6345a752762.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95e3fc47ca165b84570a",
+    "url": "/static/css/main.17c20538.chunk.css"
+  },
+  {
+    "revision": "95e3fc47ca165b84570a",
+    "url": "/static/js/main.3c5b44f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "b2dfdf94613e1f4d2d0ecc0e11ced34c",
+    "url": "/index.html"
+  }
+];
